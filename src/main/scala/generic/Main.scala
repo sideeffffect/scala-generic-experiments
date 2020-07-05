a0bc1b336080505a8a3371a548ef49903475aad1
Ondra Pelech <ondra.pelech@gmail.com>
2020-07-05 22:36:12 +0200

    Generic.Representation

--- src/main/scala/generic/Main.scala
+++ src/main/scala/generic/Main.scala
@@ -19,9 +19,14 @@ object Tree {
     def apply[A](l: Tree[A], r: Tree[A]): Tree[A] = new Branch(l, r)
   }
 
-  type GenericRep[A] = Either[
-    Generic.Con[Generic.Wrap[A]],
-    Generic.Con[(Generic.Wrap[Tree[A]], Generic.Wrap[Tree[A]])]
+  type GenericRep[A] = Generic.Representation.Sum[
+    Generic.Representation.Con[Generic.Representation.Atom[A]],
+    Generic.Representation.Con[
+      Generic.Representation.Product[
+        Generic.Representation.Atom[Tree[A]],
+        Generic.Representation.Atom[Tree[A]]
+      ]
+    ]
   ]
   implicit def generic[A]: Generic[Tree[A]] { type Rep = GenericRep[A] } =
     new Generic[Tree[A]] {
@@ -29,24 +34,46 @@ object Tree {
 
       override def from(a: Tree[A]): Rep =
         a match {
-          case Leaf(x) => Left(Generic.Con("Leaf", Generic.Wrap(x)))
+          case Leaf(x) =>
+            Generic.Representation.Sum.Left(
+              Generic.Representation.Con("Leaf", Generic.Representation.Atom(x))
+            )
           case Branch(l, r) =>
-            Right(Generic.Con("Branch", (Generic.Wrap(l), Generic.Wrap(r))))
+            Generic.Representation.Sum.Right(
+              Generic.Representation.Con(
+                "Branch",
+                Generic.Representation.Product(
+                  Generic.Representation.Atom(l),
+                  Generic.Representation.Atom(r)
+                )
+              )
+            )
         }
 
       override def to(rep: Rep): Tree[A] =
         rep match {
-          case Left(Generic.Con(_, Generic.Wrap(value))) =>
+          case Generic.Representation.Sum.Left(
+                Generic.Representation
+                  .Con(_, Generic.Representation.Atom(value))
+              ) =>
             Leaf(value)
-          case Right(Generic.Con(_, (Generic.Wrap(l), Generic.Wrap(r)))) =>
+          case Generic.Representation.Sum.Right(
+                Generic.Representation.Con(
+                  _,
+                  Generic.Representation.Product(
+                    Generic.Representation.Atom(l),
+                    Generic.Representation.Atom(r)
+                  )
+                )
+              ) =>
             Branch(l, r)
         }
     }
 
   implicit def eq[A](implicit eqA: Eq[A]): Eq[Tree[A]] = {
     (x: Tree[A], y: Tree[A]) =>
-      GEq.eq(x, y)
-    //(generic)(GEq.either(GEq.wrap(eqA), GEq.pair(GEq.wrap(eq(eqA)), GEq.wrap(eq(eqA)))))
+      GEq.eq[Tree[A], Tree.GenericRep[A]](x, y)
+    // (generic, GEq.either(GEq.con(GEq.wrap(eqA)), GEq.con(GEq.pair(GEq.wrap(eq(eqA)), GEq.wrap(eq(eqA))))))
   }
 
   def conName[A](x: Tree[A]): String =
@@ -55,7 +82,7 @@ object Tree {
 }
 
 trait Generic[A] {
-  type Rep
+  type Rep <: Generic.Representation
 
   def from(a: A): Rep
 
@@ -64,18 +91,38 @@ trait Generic[A] {
 
 object Generic {
 
-  final case class Wrap[A](unwrap: A) extends AnyVal
+  sealed trait Representation extends Product with Serializable
 
-  final case class Con[A](name: String, value: A)
+  final object Representation {
+
+    sealed trait Sum[+A <: Representation, +B <: Representation]
+        extends Representation
+    final object Sum {
+      final case class Left[A <: Representation](l: A) extends Sum[A, Nothing]
+      final case class Right[B <: Representation](r: B) extends Sum[Nothing, B]
+    }
+
+    final case class Product[+A <: Representation, +B <: Representation](
+        _1: A,
+        _2: B
+    ) extends Representation
+
+    final case object Unit extends Representation
+
+    final case class Atom[+A](unwrap: A) extends Representation
+
+    final case class Con[+A <: Representation](name: String, value: A)
+        extends Representation
+  }
 
 }
 
-trait GEq[A] {
+trait GEq[A <: Generic.Representation] {
   def geq(x: A, y: A): Boolean
 }
 
 object GEq {
-  def eq[A, R](
+  def eq[A, R <: Generic.Representation](
       x: A,
       y: A
   )(implicit generic: Generic[A] { type Rep = R }, geq: GEq[R]): Boolean = {
@@ -83,28 +130,57 @@ object GEq {
 
   }
 
-  implicit def either[A, B](implicit
+  implicit def either[A <: Generic.Representation, B <: Generic.Representation](
+      implicit
       geqA: GEq[A],
       geqB: GEq[B]
-  ): GEq[Either[A, B]] =
-    (x: Either[A, B], y: Either[A, B]) =>
+  ): GEq[Generic.Representation.Sum[A, B]] =
+    (
+        x: Generic.Representation.Sum[A, B],
+        y: Generic.Representation.Sum[A, B]
+    ) =>
       (x, y) match {
-        case (Left(x), Left(y))   => geqA.geq(x, y)
-        case (Right(x), Right(y)) => geqB.geq(x, y)
-        case _                    => false
+        case (
+              Generic.Representation.Sum.Left(x),
+              Generic.Representation.Sum.Left(y)
+            ) =>
+          geqA.geq(x, y)
+        case (
+              Generic.Representation.Sum.Right(x),
+              Generic.Representation.Sum.Right(y)
+            ) =>
+          geqB.geq(x, y)
+        case _ => false
       }
 
-  implicit def pair[A, B](implicit geqA: GEq[A], geqB: GEq[B]): GEq[(A, B)] =
-    (x: (A, B), y: (A, B)) =>
+  implicit def pair[A <: Generic.Representation, B <: Generic.Representation](
+      implicit
+      geqA: GEq[A],
+      geqB: GEq[B]
+  ): GEq[Generic.Representation.Product[A, B]] =
+    (
+        x: Generic.Representation.Product[A, B],
+        y: Generic.Representation.Product[A, B]
+    ) =>
       (x, y) match {
-        case ((x1, x2), (y1, y2)) => geqA.geq(x1, y1) && geqB.geq(x2, y2)
+        case (
+              Generic.Representation.Product(x1, x2),
+              Generic.Representation.Product(y1, y2)
+            ) =>
+          geqA.geq(x1, y1) && geqB.geq(x2, y2)
       }
 
-  implicit def wrap[A](implicit eqA: Eq[A]): GEq[Generic.Wrap[A]] =
-    (x: Generic.Wrap[A], y: Generic.Wrap[A]) => eqA.eqv(x.unwrap, y.unwrap)
+  implicit def wrap[A](implicit
+      eqA: Eq[A]
+  ): GEq[Generic.Representation.Atom[A]] =
+    (x: Generic.Representation.Atom[A], y: Generic.Representation.Atom[A]) =>
+      eqA.eqv(x.unwrap, y.unwrap)
 
-  implicit def con[A](implicit geqA: GEq[A]): GEq[Generic.Con[A]] =
-    (x: Generic.Con[A], y: Generic.Con[A]) => geqA.geq(x.value, y.value)
+  implicit def con[A <: Generic.Representation](implicit
+      geqA: GEq[A]
+  ): GEq[Generic.Representation.Con[A]] =
+    (x: Generic.Representation.Con[A], y: Generic.Representation.Con[A]) =>
+      geqA.geq(x.value, y.value)
 }
 
 sealed trait Color extends Product with Serializable
@@ -118,23 +194,57 @@ object Color {
   final case object Blue extends Color
 
   type GenericRep =
-    Either[Generic.Con[Unit], Either[Generic.Con[Unit], Generic.Con[Unit]]]
+    Generic.Representation.Sum[
+      Generic.Representation.Con[Generic.Representation.Unit.type],
+      Generic.Representation.Sum[Generic.Representation.Con[
+        Generic.Representation.Unit.type
+      ], Generic.Representation.Con[Generic.Representation.Unit.type]]
+    ]
   implicit val generic: Generic[Color] { type Rep = GenericRep } =
     new Generic[Color] {
       override type Rep = GenericRep
 
       override def from(a: Color): Rep =
         a match {
-          case Red   => Left(Generic.Con("Red", ()))
-          case Green => Right(Left(Generic.Con("Green", ())))
-          case Blue  => Right(Right(Generic.Con("Blue", ())))
+          case Red =>
+            Generic.Representation.Sum.Left(
+              Generic.Representation.Con("Red", Generic.Representation.Unit)
+            )
+          case Green =>
+            Generic.Representation.Sum.Right(
+              Generic.Representation.Sum.Left(
+                Generic.Representation.Con("Green", Generic.Representation.Unit)
+              )
+            )
+          case Blue =>
+            Generic.Representation.Sum.Right(
+              Generic.Representation.Sum.Right(
+                Generic.Representation.Con("Blue", Generic.Representation.Unit)
+              )
+            )
         }
 
       override def to(rep: Rep): Color =
         rep match {
-          case Left(Generic.Con(_, ()))         => Red
-          case Right(Left(Generic.Con(_, ())))  => Green
-          case Right(Right(Generic.Con(_, ()))) => Blue
+          case Generic.Representation.Sum
+                .Left(
+                  Generic.Representation.Con(_, Generic.Representation.Unit)
+                ) =>
+            Red
+          case Generic.Representation.Sum.Right(
+                Generic.Representation.Sum
+                  .Left(
+                    Generic.Representation.Con(_, Generic.Representation.Unit)
+                  )
+              ) =>
+            Green
+          case Generic.Representation.Sum.Right(
+                Generic.Representation.Sum
+                  .Right(
+                    Generic.Representation.Con(_, Generic.Representation.Unit)
+                  )
+              ) =>
+            Blue
         }
     }
 
@@ -146,45 +256,48 @@ object Color {
   // (generic, GConName.either(GConName.con, GConName.either(GConName.con, GConName.con)))
 }
 
-trait GEnum[A] {
+trait GEnum[A <: Generic.Representation] {
   def genum: List[A]
 }
 
 object GEnum {
-  def enum[A, R](implicit
+  def enum[A, R <: Generic.Representation](implicit
       generic: Generic[A] { type Rep = R },
       genum: GEnum[R]
   ): List[A] = genum.genum.map(generic.to)
 
-  implicit def unit[A]: GEnum[Unit] =
-    new GEnum[Unit] {
-      override def genum: List[Unit] = List(())
+  implicit def unit: GEnum[Generic.Representation.Unit.type] =
+    new GEnum[Generic.Representation.Unit.type] {
+      override def genum: List[Generic.Representation.Unit.type] =
+        List(Generic.Representation.Unit)
     }
 
-  implicit def either[A, B](implicit
+  implicit def either[A <: Generic.Representation, B <: Generic.Representation](
+      implicit
       genumA: GEnum[A],
       genumB: GEnum[B]
-  ): GEnum[Either[A, B]] =
-    new GEnum[Either[A, B]] {
-      override def genum: List[Either[A, B]] =
-        genumA.genum.map(Left(_)) ++ genumB.genum.map(Right(_))
+  ): GEnum[Generic.Representation.Sum[A, B]] =
+    new GEnum[Generic.Representation.Sum[A, B]] {
+      override def genum: List[Generic.Representation.Sum[A, B]] =
+        genumA.genum.map(Generic.Representation.Sum.Left(_)) ++ genumB.genum
+          .map(Generic.Representation.Sum.Right(_))
     }
 
-  implicit def con[A](implicit
+  implicit def con[A <: Generic.Representation](implicit
       genum0: GEnum[A]
-  ): GEnum[Generic.Con[A]] =
-    new GEnum[Generic.Con[A]] {
-      override def genum: List[Generic.Con[A]] =
-        genum0.genum.map(Generic.Con("<undefined>", _))
+  ): GEnum[Generic.Representation.Con[A]] =
+    new GEnum[Generic.Representation.Con[A]] {
+      override def genum: List[Generic.Representation.Con[A]] =
+        genum0.genum.map(Generic.Representation.Con("<undefined>", _))
     }
 }
 
-trait GConName[A] {
+trait GConName[A <: Generic.Representation] {
   def gconName(x: A): String
 }
 
 object GConName {
-  def conName[A, R](
+  def conName[A, R <: Generic.Representation](
       x: A
   )(implicit
       generic: Generic[A] { type Rep = R },
@@ -192,15 +305,18 @@ object GConName {
   ): String =
     gconName.gconName(generic.from(x))
 
-  implicit def either[A, B](implicit
+  implicit def either[A <: Generic.Representation, B <: Generic.Representation](
+      implicit
       gconnameA: GConName[A],
       gconnameB: GConName[B]
-  ): GConName[Either[A, B]] = {
-    case Left(value)  => gconnameA.gconName(value)
-    case Right(value) => gconnameB.gconName(value)
+  ): GConName[Generic.Representation.Sum[A, B]] = {
+    case Generic.Representation.Sum.Left(value)  => gconnameA.gconName(value)
+    case Generic.Representation.Sum.Right(value) => gconnameB.gconName(value)
   }
 
-  implicit def con[A]: GConName[Generic.Con[A]] = (x: Generic.Con[A]) => x.name
+  implicit def con[A <: Generic.Representation]
+      : GConName[Generic.Representation.Con[A]] =
+    (x: Generic.Representation.Con[A]) => x.name
 }
 
 object Main {
